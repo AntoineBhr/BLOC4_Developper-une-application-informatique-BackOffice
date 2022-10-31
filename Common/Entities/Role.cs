e23cb2c94d3a55314a20dd1006db59a957a9a8d3
Antoine Bouhier <antoine.bhr4@gmail.com>
2022-10-31 12:16:56 +0100

    Initalize Project and create all Classes + basics of services

---
+++ Common/Entities/Role.cs
@@ -0,0 +1,19 @@
+﻿using Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Entities
+{
+    public class Role : Entity
+    {
+        public Role(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
